eeebc0671bf3c4ccd3bd6d55b1981dce2078b55b
Victoria <victoriasantiagocontato@gmail.com>
2026-04-03 12:03:05 -0300

    Adicionei a página contactos

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         # FIX: Replace this email with recipient email
-        $mail_to = "akmolhosen997@gmail.com";
+        $mail_to = "geral@ccunha.pt";
         
         # Sender Data
         $name = str_replace(array("\r","\n"),array(" "," ") , strip_tags(trim($_POST["name"])));
