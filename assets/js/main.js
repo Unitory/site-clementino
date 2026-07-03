b7efc1cb905e3dcccfb5f1498c35fad97d209ff6
Victoria <victoriasantiagocontato@gmail.com>
2026-07-03 09:58:13 -0300

    hero e area de actuação

--- assets/js/main.js
+++ assets/js/main.js
@@ -665,7 +665,7 @@ gsap.utils.toArray('.wa-scaleXInUp').forEach((item) => {
 	gsap.from(item, {
 	  scaleX: 0,
 	  ease: "ease1",
-	  duration: 0.8,
+	  duration: 0.6,
 	  scrollTrigger: {
 		trigger: item,
 		start: "top 90%",
